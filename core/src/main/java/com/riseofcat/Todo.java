765b3d9d424c58798796b1dc3f8712437d46320b
d.avdeev <avdeev@tutu.ru>
2018-02-25 21:33:31 +0300

    libgdx desktop version

--- core/src/main/java/com/riseofcat/Todo.java
+++ core/src/main/java/com/riseofcat/Todo.java
@@ -1,13 +1,13 @@
 package com.riseofcat;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.riseofcat.old.JsonTest;
+//import com.riseofcat.old.JsonTest;
 
 public class Todo {
 
 private void todo() {
 	new OrthographicCamera().setToOrtho(false, 100, 100);//in resize method
-	JsonTest.test(App.log);
+//	JsonTest.test(App.log);
 }
 public static void handleOffline() {
 	//todo
