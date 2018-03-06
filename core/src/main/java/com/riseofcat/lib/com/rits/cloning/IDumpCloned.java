619f32618ea9d91263a7c7e0ed3b9f58cd377b0d
d.avdeev <avdeev@tutu.ru>
2018-03-06 22:31:05 +0300

    redundant

--- core/src/main/java/com/riseofcat/lib/com/rits/cloning/IDumpCloned.java
+++
@@ -1,9 +0,0 @@
-package com.riseofcat.lib.com.rits.cloning;
-
-import com.badlogic.gdx.utils.reflect.Field;
-
-public interface IDumpCloned {
-void startCloning(Class<?> clz);
-
-void cloning(Field field, Class<?> clz);
-}
