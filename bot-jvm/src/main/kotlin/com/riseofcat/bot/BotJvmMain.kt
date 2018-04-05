de8e615c9d339b09b3554ec8382a78bc2af216be
d.avdeev <avdeev@tutu.ru>
2018-04-05 08:40:57 +0300

    stable bot

--- bot-jvm/src/main/kotlin/com/riseofcat/bot/BotJvmMain.kt
+++ bot-jvm/src/main/kotlin/com/riseofcat/bot/BotJvmMain.kt
@@ -10,22 +10,20 @@ import com.riseofcat.share.mass.*
 import kotlin.concurrent.*
 
 fun main(vararg args:String) {
-  //todo сделать DUMMY с лёгкой моделью без вызовов getState state.tick()
   val DUMMY = true
   println("bot-jvm")
   println(LibJvm.test())
-  repeat(25) {
+  repeat(8) {
 //    val model:ClientModel = ClientModel(Conf(5000, "192.168.43.176"))
 //    val model = ClientModel(Conf(5000, "localhost"))
     val model = DummyModel(Conf(5000, "localhost"))
-    timer("client i", true, period = rnd(400, 700).toLong()) {
-      if(DUMMY) {
-        if(rnd(0,1) == 1) {
-          model.move(degreesAngle(rnd(0,360)))
-        } else {
-          model.newCar()
-        }
+    timer("client i", true, period = rnd(300, 400).toLong()) {
+      if(rnd(0,1) == 1) {
+        model.move(degreesAngle(rnd(0,360)))
+      } else {
+        model.newCar()
       }
     }
+    Thread.sleep(2000L)
   }
 }
